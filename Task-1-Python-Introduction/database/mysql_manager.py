9f81e262e892fa428663f3b530f8d5cbe4a8180f
Zakhar Smalenski <z.smolensz@gmail.com>
2023-09-05 20:32:27 +0200

    Сonnector DB creation

---
+++ Task-1-Python-Introduction/database/mysql_manager.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+
+class MySQLManager:
+
+    def __init__(self, host, user, password):
+        self.conn = None
+        self.conn = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password
+        )
+
+    # self.conn = mysql.connector.connect(host="localhost", user="root", password="1230321!", database="testdb")
+
+    def create_db(self):
+        my_cursor = self.conn.cursor()
+        my_cursor.execute("CREATE DATABASE testdb")
+
+    def show_db(self):
+        my_cursor = self.conn.cursor()
+
+        my_cursor.execute("SHOW DATABASES")
+
+        for db in my_cursor:
+            print(db)
+
+    def close_connection(self):
+        self.conn.close()
