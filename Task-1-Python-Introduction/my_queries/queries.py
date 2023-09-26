34cbe38803e73d58d5adf0e5f9de1c85d4ab61b0
Zakhar Smalenski <z.smolensz@gmail.com>
2023-09-26 14:14:14 +0200

    Add main queries.

--- Task-1-Python-Introduction/my_queries/queries.py
+++ Task-1-Python-Introduction/my_queries/queries.py
@@ -30,6 +30,38 @@ INSERT_DATA_INTO_TB_STUDENTS = """
 INSERT INTO pyIntro.students(birthday, id, name, room, sex) VALUES(%s,%s,%s,%s,%s);
 """
 
+GET_LIST_OF_ROOMS_AND_STUDENTS = """
+SELECT Rooms.name, COUNT(*) AS Count_of_Students
+FROM pyIntro.rooms Rooms JOIN pyIntro.students Students 
+ON Rooms.id = Students.room
+GROUP BY Rooms.name;
+"""
 
+AVG_AGE_OF_STUDENTS = """
+SELECT Rooms.name, AVG(DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), Students.birthday)), '%Y') + 0) AS Avg_age_of_Students
+FROM pyIntro.students Students JOIN pyIntro.rooms Rooms 
+ON Rooms.id = Students.room
+GROUP BY Rooms.name
+ORDER BY Avg_age_of_Students
+LIMIT 5;
+"""
 
+DIFF_OF_AGE = """
+SELECT Rooms.name, (MAX(DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), Students.birthday)), '%Y') + 0) - MIN(DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), Students.birthday)), '%Y') + 0)) As Diff_of_age
+FROM pyIntro.students Students JOIN pyIntro.rooms Rooms 
+ON Rooms.id = Students.room
+GROUP BY Rooms.name
+ORDER BY Diff_of_age DESC
+LIMIT 5;
+"""
 
+GET_ROOMS_ACCORDING_GENDER = """
+SELECT Rooms.name
+FROM pyIntro.students Students JOIN pyIntro.rooms Rooms 
+ON Rooms.id = Students.room
+Where Students.sex = 'M' AND Rooms.name IN (SELECT Rooms.name 
+                                            FROM pyIntro.students Students JOIN pyIntro.rooms Rooms 
+                                            ON Rooms.id = Students.room
+                                            WHERE Students.sex = 'F')
+GROUP BY Rooms.name;
+"""
